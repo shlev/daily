8172544fd79662c0c46344a4b6927e2d35cb5608
shlomi <shlomi@magic-flex.com>
2020-08-15 14:05:13 +0300

    init spring boot h2

---
+++ springboot-h2/h2-springboot/src/test/java/com/shl/key/h2springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shl.key.h2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
